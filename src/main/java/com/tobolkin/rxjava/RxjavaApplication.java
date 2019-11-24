826a4b1fe3d3c8854bfdb6a8545b7775bd1bbebf
tobantal <tobantal@gmail.com>
2019-11-24 07:11:18 +0000

    added Observable merge example

--- src/main/java/com/tobolkin/rxjava/RxjavaApplication.java
+++ src/main/java/com/tobolkin/rxjava/RxjavaApplication.java
@@ -8,14 +8,26 @@ import rx.Observable;
 @SpringBootApplication
 public class RxjavaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RxjavaApplication.class, args);
-		//System.out.println(">>>>>>>>>>>>>> Hello World!");
+    public static void main(String[] args) {
+        SpringApplication.run(RxjavaApplication.class, args);
+        // System.out.println(">>>>>>>>>>>>>> Hello World!");
 
-		 Observable.create(s -> {
-        	   	 s.onNext("Hello World!");
-			 s.onCompleted();
-       		 }).subscribe(hello -> System.out.println(hello));
-	}
+        Observable<String> obs1 = Observable.create(s -> {
+            s.onNext("Hello World!");
+            s.onNext("This is your world!");
+            s.onCompleted();
+        });
+
+        Observable<String> obs2 = Observable.create(s -> {
+            s.onNext("Hello World from SecondObservable!");
+            s.onCompleted();
+        });
+
+        Observable<String> obs3 = Observable.merge(obs1, obs2);
+
+        obs3.subscribe(System.out::println);
+
+        System.exit(0);
+    }
 
 }
