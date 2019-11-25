b94a50a266766d0f13a91c1dc07a4c3f4dcd8009
tobantal <tobantal@gmail.com>
2019-11-25 14:03:46 +0000

    cleaned code

--- src/test/java/com/tobolkin/rxjava/ch1/SimpleObservableCreationTest.java
+++ src/test/java/com/tobolkin/rxjava/ch1/SimpleObservableCreationTest.java
@@ -15,12 +15,10 @@ import static org.assertj.core.data.Index.atIndex;
 
 public class SimpleObservableCreationTest {
 
-    private static final String SOME_KEY = "FOO";
-
     @Test
-	void shouldCreateObservableFromCreateMethod() {
+    void shouldCreateObservableFromCreateMethod() {
         final List<String> result = new ArrayList<>();
-        Observable<String> obs = Observable.create(s -> {
+        final Observable<String> obs = Observable.create(s -> {
             s.onNext("Hello World!");
             s.onCompleted();
         });
@@ -31,26 +29,26 @@ public class SimpleObservableCreationTest {
     }
 
     @Test
-	public void shoulGetRemoteValue() throws Exception {
-		final Map<String, String> cache = new ConcurrentHashMap<>();
-		cache.put(SOME_KEY, "123");
+    public void shoulGetRemoteValue() throws Exception {
+        final Map<String, String> cache = new ConcurrentHashMap<>();
+        final String someKey = "foo";
+        cache.put(someKey, "123");
 
-		Observable.create(s -> {
-			s.onNext(cache.get(SOME_KEY));
-			s.onCompleted();
-		}).subscribe(value -> assertThat(value).isEqualTo("123"));
+        Observable.create(s -> {
+            s.onNext(cache.get(someKey));
+            s.onCompleted();
+        }).subscribe(value -> assertThat(value).isEqualTo("123"));
     }
 
     @Test
-	void shouldGetValueWithSleep() {
+    void shouldGetValueWithSleep() {
         final List<Integer> result = new ArrayList<>();
-
-        Observable<Integer> obs = Observable.create(s -> {
-            for(int i = 0; i < 10; i++) {
+        final Observable<Integer> obs = Observable.create(s -> {
+            for (int i = 0; i < 10; i++) {
                 try {
-					Thread.sleep(1_000);
-				} catch (InterruptedException e) {
-					// ignore
+                    Thread.sleep(1_000);
+                } catch (InterruptedException e) {
+                    // ignore
                 }
                 s.onNext(i);
             }
@@ -58,7 +56,34 @@ public class SimpleObservableCreationTest {
         });
 
         obs.subscribe(s -> result.add(s));
+
         assertThat(result).hasSize(10);
         assertThat(result).contains(4, atIndex(4));
+    }
+
+    @Test
+    void shouldMergeValuesInSync() {
+        final List<Integer> result = new ArrayList<>();
+        final Observable<Integer> obs1 = Observable.create(s -> {
+            for (int i = 0; i < 5; i++) {
+                s.onNext(i);
+            }
+            s.onCompleted();
+        });
+
+        final Observable<Integer> obs2 = Observable.create(s -> {
+            for (int i = 5; i < 10; i++) {
+                s.onNext(i);
+            }
+            s.onCompleted();
+        });
+
+        final Observable<Integer> obs3 = Observable.merge(obs1, obs2);
+        obs3.subscribe(s -> result.add(s));
+
+        assertThat(result).hasSize(10);
+        for (int i = 0; i < 10; i++) {
+            assertThat(result).contains(i, atIndex(i));
+        }
     }
 }
