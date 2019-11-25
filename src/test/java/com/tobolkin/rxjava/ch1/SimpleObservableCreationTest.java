5573b809afe20f1427c719cee7aa0ae4a582251a
tobantal <tobantal@gmail.com>
2019-11-25 14:05:27 +0000

    removed unused import

--- src/test/java/com/tobolkin/rxjava/ch1/SimpleObservableCreationTest.java
+++ src/test/java/com/tobolkin/rxjava/ch1/SimpleObservableCreationTest.java
@@ -1,7 +1,6 @@
 package com.tobolkin.rxjava.ch1;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
