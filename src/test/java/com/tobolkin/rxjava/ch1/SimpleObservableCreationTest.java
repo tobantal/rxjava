733386fb6390b0187cdfc9f48f118410be83a468
tobantal <tobantal@gmail.com>
2019-11-25 13:48:33 +0000

    added tests in ch1

--- src/test/java/com/tobolkin/rxjava/ch1/SimpleObservableCreationTest.java
+++ src/test/java/com/tobolkin/rxjava/ch1/SimpleObservableCreationTest.java
@@ -3,6 +3,8 @@ package com.tobolkin.rxjava.ch1;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 import rx.Observable;
 
@@ -12,6 +14,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.data.Index.atIndex;
 
 public class SimpleObservableCreationTest {
+
+    private static final String SOME_KEY = "FOO";
 
     @Test
 	void shouldCreateObservableFromCreateMethod() {
@@ -26,4 +30,35 @@ public class SimpleObservableCreationTest {
         assertThat(result).contains("Hello World!", atIndex(0));
     }
 
+    @Test
+	public void shoulGetRemoteValue() throws Exception {
+		final Map<String, String> cache = new ConcurrentHashMap<>();
+		cache.put(SOME_KEY, "123");
+
+		Observable.create(s -> {
+			s.onNext(cache.get(SOME_KEY));
+			s.onCompleted();
+		}).subscribe(value -> assertThat(value).isEqualTo("123"));
+    }
+
+    @Test
+	void shouldGetValueWithSleep() {
+        final List<Integer> result = new ArrayList<>();
+
+        Observable<Integer> obs = Observable.create(s -> {
+            for(int i = 0; i < 10; i++) {
+                try {
+					Thread.sleep(1_000);
+				} catch (InterruptedException e) {
+					// ignore
+                }
+                s.onNext(i);
+            }
+            s.onCompleted();
+        });
+
+        obs.subscribe(s -> result.add(s));
+        assertThat(result).hasSize(10);
+        assertThat(result).contains(4, atIndex(4));
+    }
 }
